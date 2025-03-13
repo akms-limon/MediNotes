933f99299c3942d6e4772f902fac4c70ba917b1d
A K M S Limon <akmslimon@example.com>
2025-03-14 05:18:46 +0600

    Session update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { config } from "dotenv";
 import express from "express";
 import connectToPostgres from "./database/db.js";
 import globalErrhandler from "./middlewares/globalHandler.js";
-
+import session from "express-session";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import doctorRoutes from "./routes/doctor.js";
@@ -11,10 +11,19 @@ import userRoutes from "./routes/users.js";
 const app = express();
 const port = 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Configure the session middleware
+app.use(session({
+  secret: 'sdklf8943jlkj',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 // Set up the view engine (example: EJS)
 app.set("view engine", "ejs"); 
 config();
-//
+
 app.use(express.static(__dirname, +"/public"));
 //
 // // Middleware
